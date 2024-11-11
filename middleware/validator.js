ea1de48c5482392aae672d3c6086b3b67da183c7
dat02 <datnguyendinh1311@gmail.com>
2024-11-11 18:25:23 +0700

    also update validator

--- middleware/validator.js
+++ middleware/validator.js
@@ -3,12 +3,24 @@
 const { body, param } = require('express-validator');
 
 const getTeamRule = [
-  
-  param('teamId')
+
+param('teamId')
     .isInt()
     .withMessage('team ID must be an integer'),
 
+param('memberId')
+    .isInt() 
+    .withMessage('member Id must be an integer'),
+
+param('managerId')
+    .isInt()
+    .withMessage('manager ID must be an integer')
+
 ];
+
+
+
+
 
 
 module.exports = {getTeamRule}
--- routes/teamRouter.js
+++ routes/teamRouter.js
@@ -9,9 +9,9 @@ router.get('/details', teamController.getAllDetails);
 router.get('/:teamId',getTeamRule, teamController.getTeam);
 
 router.post('/', checkRole(3),teamController.create);
-router.post('/:teamId/members', teamController.addMember);
-router.post('/:teamId/managers', checkRole(3), teamController.addManager);
-router.delete('/:teamId/members/:memberId', teamController.deleteMember);
+router.post('/:teamId/members', getTeamRule, teamController.addMember);
+router.post('/:teamId/managers', checkRole(3), getTeamRule , teamController.addManager);
+router.delete('/:teamId/members/:memberId',  teamController.deleteMember);
 // // tai sao o day k delete('/:teamId/:memberId') -> vi de phan biet xoa memberId vs managerId
 router.delete('/:teamId/managers/:managerId', checkRole(3), teamController.deleteManager);
 
